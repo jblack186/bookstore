e499602bd98977e9a4bbd1602ae854c03d76ded3
Jamison Blackwell <jamie@Jamisons-MacBook-Pro.local>
2020-11-03 15:04:31 -0500

    verifying database.sql file

--- database.sql
+++ database.sql
@@ -1,10 +1,9 @@
 -- CREATE DATABASE bookstore;
 
 CREATE TABLE users(
-  user_id uuid PRIMARY KEY DEFAULT 
-  uuid_generate_v4(),
+  user_id uuid DEFAULT uuid_generate_v4(),
   user_name VARCHAR(255) NOT NULL,
-  user_email VARCHAR(255) NOT NULL,
-  user_password VARCHAR(255) NOT NULL
+  user_email VARCHAR(255) NOT NULL UNIQUE,
+  user_password VARCHAR(255) NOT NULL,
+  PRIMARY KEY(user_id)
 );
-
