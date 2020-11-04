cd734720cc653a6097c88cfbdf1322d8fc8e28b0
Jamison Blackwell <jamie@Jamisons-MacBook-Pro.local>
2020-11-04 02:41:25 -0500

    test

--- routes/jwtAuth.js
+++ routes/jwtAuth.js
@@ -1,71 +1,22 @@
 const router = require("express").Router();
-const bcrypt = require("bcrypt");
-const pool = require("../db");
-const validInfo = require("../middleware/validInfo");
-const jwtGenerator = require("../utils/jwtGenerator");
 const authorize = require("../middleware/authorize");
+const pool = require("../db");
 
-
-router.post("/register", validInfo, async (req, res) => {
-  const { email, name, password } = req.body;
-
+router.post("/", authorize, async (req, res) => {
   try {
-    const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
-      email
-    ]);
-
-    if (user.rows.length > 0) {
-      return res.status(401).json("User already exist!");
-    }
-
-    const salt = await bcrypt.genSalt(10);
-    const bcryptPassword = await bcrypt.hash(password, salt);
-
-    let newUser = await pool.query(
-      "INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *",
-      [name, email, bcryptPassword]
-    );
-
-    const jwtToken = jwtGenerator(newUser.rows[0].user_id);
-
-    return res.json({ jwtToken });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server error");
-  }
-});
-
-router.post("/login", validInfo, async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
-      email
-    ]);
-
-    if (user.rows.length === 0) {
-      return res.status(401).json("Invalid Credential");
-    }
-
-    const validPassword = await bcrypt.compare(
-      password,
-      user.rows[0].user_password
-    );
-
-    if (!validPassword) {
-      return res.status(401).json("Invalid Credential");
-    }
-    const jwtToken = jwtGenerator(user.rows[0].user_id);
-    return res.json({ jwtToken });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server error");
-  }
-});
-
-router.post("/verify", authorize, (req, res) => {
-  try {
-    res.json(true);
+    const user = await pool.query(
+      "SELECT user_name FROM users WHERE user_id = $1",
+      [req.user.id] 
+    ); 
+    
+  //if would be req.user if you change your payload to this:
+    
+  //   function jwtGenerator(user_id) {
+  //   const payload = {
+  //     user: user_id
+  //   };
+    
+    res.json(user.rows[0]);
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server error");
