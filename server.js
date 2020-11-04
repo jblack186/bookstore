03ab727b07f2ce1f75d83f13270a8abf4ca9bb70
Jamison Blackwell <jamie@Jamisons-MacBook-Pro.local>
2020-11-04 17:44:38 -0500

    test

--- server.js
+++ server.js
@@ -22,19 +22,19 @@ app.use("/dashboard", require("./routes/dashboard"));
 app.use("/auth", require("./routes/jwtAuth"));
 
 
-// app.get("/", async (req, res) => {
-//   try {
-//     const users = await pool.query("SELECT * FROM users");
+app.get("/", async (req, res) => {
+  try {
+    const users = await pool.query("SELECT * FROM users");
 
-//     res.json(users.rows);
-//   } catch (err) {
-//     console.error(err.message);
-//   }
-// });
-//if user goes somewhere other than a predefined route then this will take them back to the homepage
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "store/build/index.html")); 
-// })
+    res.json(users.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+if user goes somewhere other than a predefined route then this will take them back to the homepage
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "store/build/index.html")); 
+})
 
 app.listen(PORT, () => {
   console.log(`Server is starting on port ${PORT}`);
