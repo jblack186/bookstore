c553f1d6480f8b6640f77f41822d0ea43b47268b
Jamison Blackwell <jamie@Jamisons-MacBook-Pro.local>
2020-11-04 15:56:42 -0500

    test deploy

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express();
 const cors = require("cors");
 const path = require("path")
 const PORT = process.env.PORT || 5000;
-const pool = require("./db");
 
 
 
@@ -22,15 +21,15 @@ app.use("/dashboard", require("./routes/dashboard"));
 // app.use("/auth", require("./routes/jwtAuth"));
 
 
-app.get("/", async (req, res) => {
-  try {
-    const users = await pool.query("SELECT * FROM users");
+// app.get("/", async (req, res) => {
+//   try {
+//     const users = await pool.query("SELECT * FROM users");
 
-    res.json(users.rows);
-  } catch (err) {
-    console.error(err.message);
-  }
-});
+//     res.json(users.rows);
+//   } catch (err) {
+//     console.error(err.message);
+//   }
+// });
 //if user goes somewhere other than a predefined route then this will take them back to the homepage
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "store/build/index.html")); 
