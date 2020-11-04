91ecbbd47443fc030173dc16eb42a3eb00176cdb
Jamison Blackwell <jamie@Jamisons-MacBook-Pro.local>
2020-11-04 03:01:20 -0500

    test

--- routes/jwtAuth.js
+++ routes/jwtAuth.js
@@ -35,41 +35,41 @@ const authorize = require("../middleware/authorize");
 //   }
 // });
 
-router.post("/login", validInfo, async (req, res) => {
-  const { email, password } = req.body;
+// router.post("/login", validInfo, async (req, res) => {
+//   const { email, password } = req.body;
 
-  try {
-    const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
-      email
-    ]);
+//   try {
+//     const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
+//       email
+//     ]);
 
-    if (user.rows.length === 0) {
-      return res.status(401).json("Invalid Credential");
-    }
+//     if (user.rows.length === 0) {
+//       return res.status(401).json("Invalid Credential");
+//     }
 
-    const validPassword = await bcrypt.compare(
-      password,
-      user.rows[0].user_password
-    );
+//     const validPassword = await bcrypt.compare(
+//       password,
+//       user.rows[0].user_password
+//     );
 
-    if (!validPassword) {
-      return res.status(401).json("Invalid Credential");
-    }
-    const jwtToken = jwtGenerator(user.rows[0].user_id);
-    return res.json({ jwtToken });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server error");
-  }
-});
+//     if (!validPassword) {
+//       return res.status(401).json("Invalid Credential");
+//     }
+//     const jwtToken = jwtGenerator(user.rows[0].user_id);
+//     return res.json({ jwtToken });
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).send("Server error");
+//   }
+// });
 
-router.post("/verify", authorize, (req, res) => {
-  try {
-    res.json(true);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server error");
-  }
-});
+// router.post("/verify", authorize, (req, res) => {
+//   try {
+//     res.json(true);
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).send("Server error");
+//   }
+// });
 
 module.exports = router;
