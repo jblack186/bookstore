6a68a8d4cd55a23f2c62c4284e3d22ece0dff72b
Jamison Blackwell <jamie@Jamisons-MacBook-Pro.local>
2020-11-04 17:19:36 -0500

    test

--- routes/jwtAuth.js
+++ routes/jwtAuth.js
@@ -1,6 +1,6 @@
 const routers = require("express").Router();
 const bcrypt = require("bcrypt");
-const jwtGenerator = require("../utils/jwtGenerator");
+// const jwtGenerator = require("../utils/jwtGenerator");
 // const pool = require("../db");
 
 
