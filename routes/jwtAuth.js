0c73d760c58b1e97dc7a6aec53d58b1ce05a820c
Jamison Blackwell <jamie@Jamisons-MacBook-Pro.local>
2020-11-04 17:07:44 -0500

    test

--- routes/jwtAuth.js
+++ routes/jwtAuth.js
@@ -1,65 +1,65 @@
 const routers = require("express").Router();
-// const bcrypt = require("bcrypt");
-// const jwtGenerator = require("../utils/jwtGenerator");
-// const pool = require("../db");
+const bcrypt = require("bcrypt");
+const jwtGenerator = require("../utils/jwtGenerator");
+const pool = require("../db");
 
 
-// router.post("/register", async (req, res) => {
-//   const { email, name, password } = req.body;
+router.post("/register", async (req, res) => {
+  const { email, name, password } = req.body;
 
-//   try {
-//     const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
-//       email
-//     ]);
+  try {
+    const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
+      email
+    ]);
 
-//     if (user.rows.length > 0) {
-//       return res.status(401).json("User already exist!");
-//     }
+    if (user.rows.length > 0) {
+      return res.status(401).json("User already exist!");
+    }
 
-//     const salt = await bcrypt.genSalt(10);
-//     const bcryptPassword = await bcrypt.hash(password, salt);
+    const salt = await bcrypt.genSalt(10);
+    const bcryptPassword = await bcrypt.hash(password, salt);
 
-//     let newUser = await pool.query(
-//       "INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *",
-//       [name, email, bcryptPassword]
-//     );
+    let newUser = await pool.query(
+      "INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *",
+      [name, email, bcryptPassword]
+    );
 
-//     const jwtToken = jwtGenerator(newUser.rows[0].user_id);
+    const jwtToken = jwtGenerator(newUser.rows[0].user_id);
 
-//     return res.json({ jwtToken });
-//   } catch (err) {
-//     console.error(err.message);
-//     res.status(500).send("Server error");
-//   }
-// });
+    return res.json({ jwtToken });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
 
-// router.post("/login", async (req, res) => {
-//   const { email, password } = req.body;
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
 
-//   try {
-//     const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
-//       email
-//     ]);
+  try {
+    const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
+      email
+    ]);
 
-//     if (user.rows.length === 0) {
-//       return res.status(401).json("Invalid Credential");
-//     }
+    if (user.rows.length === 0) {
+      return res.status(401).json("Invalid Credential");
+    }
 
-//     const validPassword = await bcrypt.compare(
-//       password,
-//       user.rows[0].user_password
-//     );
+    const validPassword = await bcrypt.compare(
+      password,
+      user.rows[0].user_password
+    );
 
-//     if (!validPassword) {
-//       return res.status(401).json("Invalid Credential");
-//     }
-//     const jwtToken = jwtGenerator(user.rows[0].user_id);
-//     return res.json({ jwtToken });
-//   } catch (err) {
-//     console.error(err.message);
-//     res.status(500).send("Server error");
-//   }
-// });
+    if (!validPassword) {
+      return res.status(401).json("Invalid Credential");
+    }
+    const jwtToken = jwtGenerator(user.rows[0].user_id);
+    return res.json({ jwtToken });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
 
 // router.post("/verify", (req, res) => {
 //   try {
