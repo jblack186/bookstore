200f0df1cc085621bd2e36fc5069df5165644f6c
Jamison Blackwell <jamie@Jamisons-MacBook-Pro.local>
2020-11-03 21:57:20 -0500

    added login info

--- store/src/Signin.js
+++ store/src/Signin.js
@@ -4,14 +4,48 @@ import Pic from './img/signin-pic.jpg';
 import Books from './img/bookonshelf .png';
 import TopBar from './TopBar';
 import Footer from './Footer';
-
+import { toast } from "react-toastify";
 
 const SignIn = () => {
+    const [inputs, setInputs] = useState({
+      email: "",
+      password: "",
+      name: ""
+    });
+  
+  const { email, password, name } = inputs;
 
+  console.log(email, name)
 
-  const submit = () => {
+  const onChange = e =>
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
 
-  }
+    const onSubmitForm = async e => {
+      e.preventDefault();
+      try {
+        const body = { email, password, name };
+        const response = await fetch(
+          "http://localhost:5000/authentication/register",
+          {
+            method: "POST",
+            headers: {
+              "Content-type": "application/json"
+            },
+            body: JSON.stringify(body)
+          }
+        );
+        const parseRes = await response.json();
+  
+        if (parseRes.jwtToken) {
+          localStorage.setItem("token", parseRes.jwtToken);
+          toast.success("Register Successfully");
+        } else {
+          toast.error(parseRes);
+        }
+      } catch (err) {
+        console.error(err.message);
+      }
+    };
 
   return (
     <div>
@@ -24,10 +58,25 @@ const SignIn = () => {
           <p>Reading can give you superpowers. Learn, imagine, and escape with us. We can't wait for you to visit our library.</p>
         </div>
         <h3>We'd love to have you. Take some time out to register.</h3>
-        <form>
-          <input placeholder='name'/>
-          <input placeholder='email' />
-          <input placeholder='password' />
+        <form onSubmit={onSubmitForm}>
+          <input type="text"
+          name="email"
+          value={email}
+          placeholder="email"
+          onChange={e => onChange(e)}
+          className="form-control my-3"/>
+          <input type="text"
+          name="name"
+          value={name}
+          placeholder="name"
+          onChange={e => onChange(e)}
+          className="form-control my-3" />
+          <input type="password"
+          name="password"
+          value={password}
+          placeholder="password"
+          onChange={e => onChange(e)}
+          className="form-control my-3"/>
           <button>Register</button>
         </form>
       </section>
