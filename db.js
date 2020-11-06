a780372da51e588b321ab24ff518008dfad470f6
Jamison Blackwell <jamie@Jamisons-MacBook-Pro.local>
2020-11-05 19:59:44 -0500

    tst deploy

--- db.js
+++ db.js
@@ -7,6 +7,7 @@ const devConfig = {
   host: process.env.PG_HOST,
   database: process.env.PG_DATABASE,
   port: process.env.PG_PORT,
+  jwtSecret: process.env.jwtSecret
 };
 
 // const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
